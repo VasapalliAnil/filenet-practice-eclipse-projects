acbacdfe75321f66b2cfdf20e6466162a168e0b1
VasapalliAnil <vasapallianil@gmail.com>
2024-05-11 23:43:29 +0530

    Added the PE API Practice Java Project

---
+++ PEPractice/src/com/anil/PEMain.java
@@ -0,0 +1,10 @@
+package com.anil;
+
+public class PEMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inside PE Main class");
+	}
+
+}
