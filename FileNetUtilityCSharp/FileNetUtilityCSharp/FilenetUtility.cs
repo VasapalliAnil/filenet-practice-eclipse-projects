68d709897cd5a2a740e7fa8c5508de101395389a
anil vasapalli <vasapallianil7@gmail.com>
2024-06-15 07:01:01 +0000

    Added the Filenet Utility in .NET

---
+++ FileNetUtilityCSharp/FileNetUtilityCSharp/FilenetUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileNetUtilityCSharp
+{
+    class FilenetUtility
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Filenet Utility");
+        }
+    }
+}
