77fc81d5d78e246a14d545388a5678c5c9f1ecfd
anil vasapalli <vasapallianil7@gmail.com>
2024-06-15 07:37:11 +0000

    commiting the updated code

--- FileNetUtilityCSharp/FileNetUtilityCSharp/FilenetUtility.cs
+++ FileNetUtilityCSharp/FileNetUtilityCSharp/FilenetUtility.cs
@@ -1,16 +1,48 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using FileNet.Api.Authentication;
+using FileNet.Api.Core;
+using FileNet.Api.Util;
 
 namespace FileNetUtilityCSharp
 {
     class FilenetUtility
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Filenet Utility");
+            try
+            {
+                // to bypass the ssl ccertificate check
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+                // Set connection parameters; substitute for the placeholders.
+                String uri = "https://ibmdemo16:9443/wsi/FNCEWS40MTOM/";
+                String username = "p8admin";
+                String password = "Password1";
+
+                // Get client context.
+                IConnection conn = Factory.Connection.GetConnection(uri);
+                UsernameCredentials creds = new UsernameCredentials(username, password);
+                ClientContext.SetProcessCredentials(creds);
+
+                // Get default domain.
+                IDomain domain = Factory.Domain.FetchInstance(conn, null, null);
+               Console.WriteLine("Domain: " + domain.Name);
+
+                // Get object stores for domain.
+                foreach (IObjectStore store in domain.ObjectStores)
+                {
+                    Console.WriteLine("Object store: " + store.Name);
+                }
+                Console.WriteLine("Connection to Content Platform Engine successful");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
         }
     }
 }
