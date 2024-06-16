b8753614742c1e39ac4f5e438809c2ad815807f5
anil vasapalli <vasapallianil7@gmail.com>
2024-06-16 17:21:32 +0000

    Updated the code to run the extraction in a separate thread not on UI Thread to fix the unresponsive Form issue

--- FileNetWindowsForms/FileNetWindowsForms/Form1.Designer.cs
+++ FileNetWindowsForms/FileNetWindowsForms/Form1.Designer.cs
@@ -29,13 +29,114 @@ namespace FileNetWindowsForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.url = new System.Windows.Forms.Label();
+            this.urlInput = new System.Windows.Forms.TextBox();
+            this.userName = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.userNameInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.getCount = new System.Windows.Forms.Button();
+            this.countDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.AutoSize = true;
+            this.url.Location = new System.Drawing.Point(35, 13);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(56, 13);
+            this.url.TabIndex = 0;
+            this.url.Text = "MTOM Url";
+            this.url.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // urlInput
+            // 
+            this.urlInput.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.urlInput.Location = new System.Drawing.Point(121, 6);
+            this.urlInput.Name = "urlInput";
+            this.urlInput.Size = new System.Drawing.Size(100, 20);
+            this.urlInput.TabIndex = 1;
+            // 
+            // userName
+            // 
+            this.userName.AutoSize = true;
+            this.userName.Location = new System.Drawing.Point(38, 49);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(57, 13);
+            this.userName.TabIndex = 2;
+            this.userName.Text = "UserName";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(312, 49);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(53, 13);
+            this.password.TabIndex = 3;
+            this.password.Text = "Password";
+            // 
+            // userNameInput
+            // 
+            this.userNameInput.Location = new System.Drawing.Point(121, 46);
+            this.userNameInput.Name = "userNameInput";
+            this.userNameInput.Size = new System.Drawing.Size(100, 20);
+            this.userNameInput.TabIndex = 4;
+            // 
+            // passwordInput
+            // 
+            this.passwordInput.Location = new System.Drawing.Point(405, 46);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.Size = new System.Drawing.Size(100, 20);
+            this.passwordInput.TabIndex = 5;
+            // 
+            // getCount
+            // 
+            this.getCount.Location = new System.Drawing.Point(41, 113);
+            this.getCount.Name = "getCount";
+            this.getCount.Size = new System.Drawing.Size(75, 23);
+            this.getCount.TabIndex = 6;
+            this.getCount.Text = "Get Count";
+            this.getCount.UseVisualStyleBackColor = true;
+            this.getCount.Click += new System.EventHandler(this.getCountClick);
+            // 
+            // countDisplay
+            // 
+            this.countDisplay.Location = new System.Drawing.Point(173, 115);
+            this.countDisplay.Name = "countDisplay";
+            this.countDisplay.ReadOnly = true;
+            this.countDisplay.Size = new System.Drawing.Size(100, 20);
+            this.countDisplay.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countDisplay);
+            this.Controls.Add(this.getCount);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.userNameInput);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.urlInput);
+            this.Controls.Add(this.url);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label url;
+        private System.Windows.Forms.TextBox urlInput;
+        private System.Windows.Forms.Label userName;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox userNameInput;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Button getCount;
+        private System.Windows.Forms.TextBox countDisplay;
     }
 }
 
--- FileNetWindowsForms/FileNetWindowsForms/Form1.cs
+++ FileNetWindowsForms/FileNetWindowsForms/Form1.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FileNet.Api.Authentication;
+using FileNet.Api.Collection;
+using FileNet.Api.Core;
+using FileNet.Api.Query;
+using FileNet.Api.Util;
 
 namespace FileNetWindowsForms
 {
@@ -15,6 +23,117 @@ namespace FileNetWindowsForms
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void getCountClick(object sender1, EventArgs e)
+        {
+
+
+            Console.WriteLine("Button Clicked");
+            try
+            {
+                // to bypass the ssl ccertificate check
+                ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+                // Set connection parameters; substitute for the placeholders.
+                String uri = "https://ibmdemo16:9443/wsi/FNCEWS40MTOM/";
+                String username = "p8admin";
+                String password = "Password1";
+                countDisplay.Text = "Connecting to FileNet";
+                // Get client context.
+                IConnection conn = Factory.Connection.GetConnection(uri);
+                UsernameCredentials creds = new UsernameCredentials(username, password);
+                ClientContext.SetProcessCredentials(creds);
+
+                // Get default domain.
+                IDomain domain = Factory.Domain.FetchInstance(conn, null, null);
+                Console.WriteLine("Domain: " + domain.Name);
+                countDisplay.Text = "Connection Success";
+                // Get object stores for domain.
+                foreach (IObjectStore store in domain.ObjectStores)
+                {
+                    Console.WriteLine("Object store: " + store.Name);
+                }
+               
+                Console.WriteLine("Connection to Content Platform Engine successful");
+                IObjectStore os = Factory.ObjectStore.FetchInstance(domain, "Case Target", null);
+                Console.WriteLine(os.Name);
+                await ExtractDocumentsAsync(os);
+
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
+
+           
+           
+            Console.WriteLine("Done");
+            
+
+        }
+        private async Task ExtractDocumentsAsync(IObjectStore os)
+        {
+            // Connect to external repository and extract documents
+            // This is where your lengthy operation should happen
+            await Task.Run(() =>
+            {
+                // Code to connect to external repository and extract documents
+                // Replace this with your actual extraction logic
+                getDocument(os);
+            });
+
+            // After extraction is complete (if needed), update UI or do other tasks
+            MessageBox.Show("Document extraction complete!");
+        }
+
+        public  void getDocument(IObjectStore os)
+        {
+
+            SearchSQL sqlObject = new SearchSQL();
+
+            string myClassName = "PropertyTemplate";
+            //sqlObject.SetSelectList("DocumentTitle");
+            sqlObject.SetFromClauseInitialValue(myClassName, null, true);
+
+           // string containsExpression = "'FileNet'";
+           // sqlObject.SetContainsRestriction(myClassName, containsExpression);
+
+            // Displays the SQL statement.
+            Console.WriteLine("SQL: " + sqlObject.ToString());
+
+            // Executes the content search.
+            SearchScope searchScope = new SearchScope(os);
+            IRepositoryRowSet rowSet = searchScope.FetchRows(sqlObject, null, null, true);
+
+            IEnumerator iterator = rowSet.GetEnumerator();
+            int count = 0;
+            while (iterator.MoveNext())
+            {
+                count++;
+                Console.WriteLine(count);
+                Thread.Sleep(1000);
+                // countDisplay.Text = count.ToString();
+                UpdateCount(count);
+            }
+            Console.WriteLine("Total count :" + count);
+        }
+        private void UpdateCount(int count)
+        {
+            if (countDisplay.InvokeRequired)
+            {
+                // We are not on the UI thread, invoke the update
+                countDisplay.Invoke((MethodInvoker)(() => UpdateCount(count)));
+            }
+            else
+            {
+                // Update the textbox with the count
+                countDisplay.Text = count.ToString();
+            }
         }
     }
 }
