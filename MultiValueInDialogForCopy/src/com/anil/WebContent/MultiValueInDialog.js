b8ca0b444ca669c4eed2758b353d757c82c454d7
VasapalliAnil <vasapallianil@gmail.com>
2024-06-23 15:39:12 +0530

    committing the Multi Value viewing plugin

---
+++ MultiValueInDialogForCopy/src/com/anil/WebContent/MultiValueInDialog.js
@@ -0,0 +1,40 @@
+require(["dojo/_base/lang","dojo/on", "dojo/dom","dojo/aspect", "pvr/widget/editors/_FormattedDropDownEditor","dijit/Dialog","dijit/form/Button"], 
+		function(lang, on,dom,aspect, formattedDropDownEditor,Dialog,Button) {
+
+    /* This below code is for populating the props when we use OOTB Add document using entry template */
+        aspect.after(pvr.widget.editors._DropDownEditor.prototype, "postCreate", function() {
+		//debugger;
+			if(this.description == "Country names"){
+				//debugger;
+				var containerNode=this.getParent().domNode;
+					// Create a dialog box
+				
+				 
+				// Create a button and place it beside the dropdown button
+				var copyButton = new Button({
+					
+					label: "Copy",
+					onClick: function() {
+						this.domNode.style="background: url('images/view-eye.png') no-repeat;";
+                    var label = this.getParent().domNode.childNodes[1].innerText;
+                    
+                    
+                    var messageDialog = new ecm.widget.dialog.BaseDialog();
+					messageDialog.setTitle("Value");
+					messageDialog.setMessage(label,"info");
+						messageDialog.show();
+
+
+					}
+				}, "copyButton");
+
+				// set the style of Copy Button
+				copyButton.domNode.style="margin-left:25px; width: 24px; height: 24px;background: url('images/hidden-eye.png') no-repeat;";
+				containerNode.appendChild(copyButton.domNode);
+			}
+        });
+	
+	
+    
+});
+
