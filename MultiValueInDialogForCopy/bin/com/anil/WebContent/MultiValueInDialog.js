1cea416cffec97100b3896e8147ef6a044e3240a
VasapalliAnil <vasapallianil@gmail.com>
2024-06-23 16:57:57 +0530

    commiting the updated plugin

--- MultiValueInDialogForCopy/bin/com/anil/WebContent/MultiValueInDialog.js
+++ MultiValueInDialogForCopy/bin/com/anil/WebContent/MultiValueInDialog.js
@@ -4,7 +4,7 @@ require(["dojo/_base/lang","dojo/on", "dojo/dom","dojo/aspect", "pvr/widget/edit
     /* This below code is for populating the props when we use OOTB Add document using entry template */
         aspect.after(pvr.widget.editors._DropDownEditor.prototype, "postCreate", function() {
 		//debugger;
-			if(this.description == "Country names"){
+			if(this.description == "Country names" || this.description == "Phone Numbers"){
 				//debugger;
 				var containerNode=this.getParent().domNode;
 					// Create a dialog box
@@ -32,10 +32,10 @@ require(["dojo/_base/lang","dojo/on", "dojo/dom","dojo/aspect", "pvr/widget/edit
 
 					containerNode.appendChild(copyButton.domNode);
 					if(containerNode.childNodes[2].childNodes[0] != undefined){
-						containerNode.childNodes[2].childNodes[0].style="border:none;outline:none;background-color:transparent;"
+						containerNode.childNodes[2].childNodes[0].style="border:none;outline:none;background-color:transparent;";
 					}
 					else{
-						containerNode.childNodes[2].style="border:none;outline:none;background-color:transparent;"
+						containerNode.childNodes[2].style="border:none;outline:none;background-color:transparent;";
 					}
 					
 			}
