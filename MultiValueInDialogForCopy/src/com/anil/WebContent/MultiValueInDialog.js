f5124f36a07cc54eac4be4b3c8e08c7cd9f923e4
VasapalliAnil <vasapallianil@gmail.com>
2024-07-04 17:52:00 +0530

    updated debugger

--- MultiValueInDialogForCopy/src/com/anil/WebContent/MultiValueInDialog.js
+++ MultiValueInDialogForCopy/src/com/anil/WebContent/MultiValueInDialog.js
@@ -4,6 +4,7 @@ require(["dojo/_base/lang","dojo/on", "dojo/dom","dojo/aspect", "pvr/widget/edit
     /* This below code is for populating the props when we use OOTB Add document using entry template */
         aspect.after(pvr.widget.editors._DropDownEditor.prototype, "postCreate", function() {
 		debugger;
+		console.log("inside multi value plugin");
 			if(this.description == "Country names"  || this.description == "Phone Numbers"){
 				//debugger;
 				var containerNode=this.getParent().domNode;
