c0787089d55b8b871e20b0bc9dc96ba02bc4c74c
VasapalliAnil <vasapallianil@gmail.com>
2024-07-05 11:11:15 +0530

    Aadded the paste button

--- MultiValueInDialogForCopy/src/com/anil/WebContent/MultiValueInDialog.js
+++ MultiValueInDialogForCopy/src/com/anil/WebContent/MultiValueInDialog.js
@@ -1,48 +1,106 @@
-require(["dojo/_base/lang","dojo/on", "dojo/dom","dojo/aspect", "pvr/widget/editors/_FormattedDropDownEditor","dijit/Dialog","dijit/form/Button"], 
-		function(lang, on,dom,aspect, formattedDropDownEditor,Dialog,Button) {
+require(
+		[ "dojo/_base/lang", "dojo/on", "dojo/dom", "dojo/aspect",
+				"pvr/widget/editors/_FormattedDropDownEditor", "dijit/Dialog",
+	            "dijit/form/Button",
+	            "dojo/dom-construct",
+	            "dojo/on" ],
+		function(lang, on, dom, aspect, formattedDropDownEditor, Dialog, Button,domConstruct,on) {
 
-    /* This below code is for populating the props when we use OOTB Add document using entry template */
-        aspect.after(pvr.widget.editors._DropDownEditor.prototype, "postCreate", function() {
-		debugger;
-		console.log("inside multi value plugin");
-			if(this.description == "Country names"  || this.description == "Phone Numbers"){
-				//debugger;
-				var containerNode=this.getParent().domNode;
-					// Create a dialog box
-				
-				 
-				// Create a button and place it beside the dropdown button
-				var copyButton = new Button({
-					
-					label: "Copy",
-					iconClass: "copy-button",
-					showLabel: false,
-					onClick: function() {
-					console.log("Button Clicked");
-					// label value
-					var label = this.getParent().domNode.childNodes[1].innerText;
-					// create a message dialog
-                    var messageDialog = new ecm.widget.dialog.BaseDialog();
-					messageDialog.setTitle("Value");
-					messageDialog.setMessage(label,"info");
-					messageDialog.show();
+			/*
+			 * This below code is for populating the props when we use OOTB Add
+			 * document using entry template
+			 */
+			aspect
+					.after(
+							pvr.widget.editors._DropDownEditor.prototype,
+							"postCreate",
+							function() {
+								debugger;
+								console.log("inside multi value plugin");
+								// if(this.description == "Country names" ||
+								// this.description == "Phone Numbers"){
+								if (this.property.controller.id == "MobileNumbers"
+										|| this.property.controller.id == "PhoneNumbers") {
+									// debugger;
+									var containerNode = this.getParent().domNode;
+									// Create a dialog box
 
+									// Create a button and place it beside the
+									// dropdown button
+									var copyButton = new Button(
+											{
 
-					}
-				}, "copyButton");
+												label : "Copy",
+												iconClass : "copy-button",
+												showLabel : false,
+												onClick : function() {
+													console
+															.log(" Copy Button Clicked");
+													// label value
+													var label = this
+															.getParent().domNode.childNodes[1].innerText;
+													// create a message dialog
+													var messageDialog = new ecm.widget.dialog.BaseDialog();
+													messageDialog
+															.setTitle("Value");
+													messageDialog.setMessage(
+															label, "info");
+													messageDialog.show();
 
-					containerNode.appendChild(copyButton.domNode);
-					if(containerNode.childNodes[2].childNodes[0] != undefined){
-						containerNode.childNodes[2].childNodes[0].style="border:none;outline:none;background-color:transparent;";
-					}
-					else{
-						containerNode.childNodes[2].style="border:none;outline:none;background-color:transparent;";
-					}
-					
-			}
-        });
-	
-	
-    
-});
+												}
+											}, "copyButton");
 
+									// Create a button and place it beside the
+									// dropdown button
+									var pasteButton = new Button(
+											{
+
+												label : "Paste",
+												iconClass : "paste-button",
+												showLabel : false,
+												onClick : function() {
+													console.log("Paste Button Clicked");
+													myDialog.show();
+													
+												}
+											}, "pasteButton");
+									
+									// Create the Dialog
+						            var myDialog = new Dialog({
+						                title: "Add Content",
+						                content: ' <div><textarea id="userTextArea" rows="10" cols="30"></textarea><br> <button id="okButton">OK</button> </div>',
+						                style: "width: 300px;"
+						            });
+
+						            // Attach event handler to the OK button inside the Dialog
+						            on(myDialog, "show", function() {
+						                var okButton = document.getElementById("okButton");
+						                var userTextArea = document.getElementById("userTextArea");
+						                on(okButton, "click", function() {
+						                    var userInput = userTextArea.value;
+						                    console.log("User input: " + userInput);
+						                    myDialog.hide();
+						                });
+						            });
+
+						            
+									// add the copy and paste button beside the
+									// DropDownListEditor
+									containerNode
+											.appendChild(copyButton.domNode);
+									if (containerNode.childNodes[2].childNodes[0] != undefined) {
+										containerNode.childNodes[2].childNodes[0].style = "border:none;outline:none;background-color:transparent;";
+									} else {
+										containerNode.childNodes[2].style = "border:none;outline:none;background-color:transparent;";
+									}
+
+									if (containerNode.childNodes[3].childNodes[0] != undefined) {
+										containerNode.childNodes[3].childNodes[0].style = "border:none;outline:none;background-color:transparent;";
+									} else {
+										containerNode.childNodes[3].style = "border:none;outline:none;background-color:transparent;";
+									}
+
+								}
+							});
+
+		});
