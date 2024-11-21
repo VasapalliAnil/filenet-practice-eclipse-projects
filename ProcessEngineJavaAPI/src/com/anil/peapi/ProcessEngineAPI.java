ceaf9928c0afa0c21cea9d10a10fea4f217061a6
Anil Vasapalli <48754336+VasapalliAnil@users.noreply.github.com>
2024-11-21 11:18:01 +0000

    Update ProcessEngineAPI.java

--- ProcessEngineJavaAPI/src/com/anil/peapi/ProcessEngineAPI.java
+++ ProcessEngineJavaAPI/src/com/anil/peapi/ProcessEngineAPI.java
@@ -3,13 +3,14 @@ package com.anil.peapi;
 import filenet.vw.api.VWSession;
 
 public class ProcessEngineAPI {
-	private static final String CE_URI = "http://10.254.8.22:9081/wsi/FNCEWS40MTOM";
-	private static final String USER_NAME = "deadmin";
-	private static final String PASSWORD = "Password123";
-	public static void main(String[] args) {
+	private static final String CE_URI = "http://ibmdemo16:9080/wsi/FNCEWS40MTOM";
+	private static final String USER_NAME = "P8admin";
+	private static final String PASSWORD = "Password1";
+	private static final String CODEMODULE_ID="{60384E93-0000-C416-AD81-F2281118CA55}";
+	public static void main(String[] args) throws Exception {
 		
 		// TODO Auto-generated method stub
-		String connectionPoint="TOS_Default_ConnPt";
+		String connectionPoint="WFCP";
 		VWSession vwSession=PEConnection.getVWSession( CE_URI, USER_NAME, PASSWORD, connectionPoint);
 		// process Engine Utility
 		ProcessEngineUtility peu=new ProcessEngineUtility();
@@ -20,7 +21,8 @@ public class ProcessEngineAPI {
 		//peu.getWorkItemInRosterUsingCustomProperty( vwSession,"CreditCardApplication", "CCA_ApplicationNumber","HDFC_000000160003");
 		//peu.getWorkItemInRosterUsingCustomProperty( vwSession,"CreditCardApplication", "F_WobNum","6B944B0CF84B5241BDF2C05F760E5FAA");
 		//peu.unlockWorkItems( vwSession,"CreditCardApplication", "F_WobNum","6B944B0CF84B5241BDF2C05F760E5FAA");
-		peu.getCurrentQueue( vwSession,"CreditCardApplication", "F_WobNum","A125ADEB86EEA244944CA7F4C57FD920");
+		//peu.getCurrentQueue( vwSession,"CreditCardApplication", "F_WobNum","A125ADEB86EEA244944CA7F4C57FD920");
+		peu.updateCQWithCodeModule(vwSession,CODEMODULE_ID,"HelloWorld");
 		
 	}
 
